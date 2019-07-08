8b25b8ade14307d29e59bd69286a6e60ebfc8814
LeoKingMnM <1502630424@qq.com>
2019-07-08 11:20:42 +0800

    未完成的跳转add页

--- src/router.js
+++ src/router.js
@@ -8,16 +8,22 @@ Vue.use(VueRouter);
 import HeroList from './views/heroes/HeroList.vue';
 import WeaponList from './views//wapons/WeaponList.vue';
 import EquipList from './views//equips/EquipList.vue';
+//导入添加组件
+import HeroAdd from './views/heroes/HeroAdd.vue'
 
 
 //创建路由对象
 export default new VueRouter ({
-  linkExactActiveClass:'active',
+  linkActiveClass:'active',
+  
   //配置路由规则
   routes:[
     {name:'home',path:'/',redirect:'heroes'},
     {name:'heroes',path:'/heroes',component:HeroList},
     {name:'wapons',path:'/wapons',component:WeaponList},
     {name:'equips',path:'/equips',component:EquipList},
+    {name:'heroesadd',path:'/heroes/add',component:HeroAdd},
   ]
 })
+
+//导出路由
