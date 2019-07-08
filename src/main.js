eb39c17d8a869a89a11eef372eb7d4c3f6191f81
LeoKingMnM <1502630424@qq.com>
2019-07-08 08:52:45 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+//入口
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+//引入样式
+import './assets/bootstrap/css/bootstrap.min.css'
+import './assets/css/index.css'
 
 new Vue({
   render: h => h(App),
