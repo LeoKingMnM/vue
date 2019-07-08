98a9681f6fb423c489bf2bc1f921040b2a7a68aa
LeoKingMnM <1502630424@qq.com>
2019-07-08 10:12:26 +0800

    路由管理

--- src/main.js
+++ src/main.js
@@ -13,16 +13,28 @@ import VueRouter from 'vue-router';
 //配置vue插件vue-souter
 Vue.use(VueRouter);
 
+//导入英雄列组件
+import HeroList from './views/heroes/HeroList.vue';
+import WeaponList from './views//wapons/WeaponList.vue';
+import EquipList from './views//equips/EquipList.vue';
+
+
 //创建路由对象
 const router = new VueRouter ({
   //配置路由规则
   routes:[
-
+    {name:'home',path:'/',redirect:'heroes'},
+    {name:'heroes',path:'/heroes',component:HeroList},
+    {name:'wapons',path:'/wapons',component:WeaponList},
+    {name:'equips',path:'/equips',component:EquipList},
   ]
 })
 
 new Vue({
   render: h => h(App),
-  //皮质路由对象
+  //配置路由对象
   router,
+  HeroList,
+  WeaponList,
+  EquipList,
 }).$mount('#app')
